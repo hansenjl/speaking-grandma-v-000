813786bcebe42d47e84c02a74cf841e2db57a414
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-05-14 14:36:58 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -12,7 +12,8 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.upcase == phrase       
      return "NO, NOT SINCE 1938!"
- 
+ else 
+   return " HUH?! SPEAK UP, SONNY!"
   end
    
 end
